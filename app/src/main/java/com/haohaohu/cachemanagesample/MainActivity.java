5ee88183e9531d05dd89f09c9b3360f0eae67e3f
荣浩 <ronghao3508@gmail.com>
2018-02-01 09:20:18 +0800

    优化测试代码

--- app/src/main/java/com/haohaohu/cachemanagesample/MainActivity.java
+++ app/src/main/java/com/haohaohu/cachemanagesample/MainActivity.java
@@ -97,9 +97,7 @@ public class MainActivity extends AppCompatActivity {
         CacheObserver.getInstance().addObserver("key1", new IDataChangeListener() {
             @Override
             public void onDataChange(String key, String str) {
-                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
-                Gson gson = new Gson();
-                Test test = gson.fromJson(str, Test.class);
+                Toast.makeText(MainActivity.this, key + "：" + str, Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -108,77 +106,94 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.main_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Test key5Test = CacheUtil.get("key5", Test.class);
+
+                String key1Value = CacheUtil.get("key1");
+                String key2Value = CacheUtil.get("key2", true);
+                String key3Value = CacheUtil.get("key3");
+                String key4Value = CacheUtil.get("key4", true);
+                Test key5Test = CacheUtil.get("key5", Test.class);//可能为null
+                String key5Value = key5Test == null ? "" : key5Test.toString();
                 Test key6Test = CacheUtil.get("key6", Test.class, true);
+                String key6Value = key6Test == null ? "" : key6Test.toString();
+                String key7Value = CacheUtil.get("key7");
+                String key8Value = CacheUtil.get("key8", true);
+                String key9Value = CacheUtil.get("key9");
+                String key10Value = CacheUtil.get("key10", true);
+                String key11Value = CacheUtil.get("key11");
+                String key12Value = CacheUtil.get("key12", true);
+                String key13Value = CacheUtil.get("key13");
                 Test key14Test = CacheUtil.get("key14", Test.class);
+                String key14Value = key14Test == null ? "" : key14Test.toString();
+                String key15Value = CacheUtil.get("key15", true);
                 Test key16Test = CacheUtil.get("key16", Test.class, true);
-                String strKey5 = key5Test == null ? "" : key5Test.toString();
-                String strKey6 = key6Test == null ? "" : key6Test.toString();
-                String strKey14 = key14Test == null ? "" : key14Test.toString();
-                String strKey16 = key16Test == null ? "" : key16Test.toString();
+                String key16Value = key16Test == null ? "" : key16Test.toString();
+                String key17Value = CacheUtil.get(CacheUtil.translateKey("key17"), true);
+                String key18Value = CacheUtil.get("key18", false);
+                Boolean key19Value =
+                        CacheUtil.get(CacheUtil.translateKey("null"), Boolean.class, false, true);
+                Test key20Value = CacheUtil.get(CacheUtil.translateKey("null1"), Test.class,
+                        new Test(1, "默认值"), true);
+
                 StringBuilder builder = new StringBuilder();
                 String value = builder.append("不加密字符串测试:")
-                        .append(check("测试数据1", CacheUtil.get("key1")))
+                        .append(check("测试数据1", key1Value))
                         .append("\n")
                         .append("加密字符串测试:")
-                        .append(check("测试数据2", CacheUtil.get("key2", true)))
+                        .append(check("测试数据2", key2Value))
                         .append("\n")
                         .append("不加密字符串测试:")
-                        .append(check("测试数据18", CacheUtil.get("key18", false)))
+                        .append(check("测试数据18", key18Value))
                         .append("\n")
                         .append("特殊字符串测试:")
-                        .append(check("~!@#$%^&*()_+{}[];':,.<>`", CacheUtil.get("key3")))
+                        .append(check("~!@#$%^&*()_+{}[];':,.<>`", key3Value))
                         .append("\n")
                         .append("加密特殊字符串测试:")
-                        .append(check("~!@#$%^&*()_+{}[];':,.<>`", CacheUtil.get("key4", true)))
+                        .append(check("~!@#$%^&*()_+{}[];':,.<>`", key4Value))
                         .append("\n")
                         .append("实体对象测试:")
-                        .append(check(new Test(1, "2").toString(), strKey5))
+                        .append(check(new Test(1, "2").toString(), key5Value))
                         .append("\n")
                         .append("加密实体对象测试:")
-                        .append(check(new Test(1, "2").toString(), strKey6))
+                        .append(check(new Test(1, "2").toString(), key6Value))
                         .append("\n")
                         .append("jsonObject对象测试:")
-                        .append(check(jsonObject.toString(), CacheUtil.get("key7")))
+                        .append(check(jsonObject.toString(), key7Value))
                         .append("\n")
                         .append("加密jsonObject对象测试:")
-                        .append(check(jsonObject.toString(), CacheUtil.get("key8", true)))
+                        .append(check(jsonObject.toString(), key8Value))
                         .append("\n")
                         .append("jsonArray对象测试:")
-                        .append(check(jsonArray.toString(), CacheUtil.get("key9")))
+                        .append(check(jsonArray.toString(), key9Value))
                         .append("\n")
                         .append("加密jsonArray对象测试:")
-                        .append(check(jsonArray.toString(), CacheUtil.get("key10", true)))
+                        .append(check(jsonArray.toString(), key10Value))
                         .append("\n")
                         .append("数字测试:")
-                        .append(check(1 + "", CacheUtil.get("key11")))
+                        .append(check(1 + "", key11Value))
                         .append("\n")
                         .append("加密数字测试:")
-                        .append(check(1 + "", CacheUtil.get("key12", true)))
+                        .append(check(1 + "", key12Value))
                         .append("\n")
                         .append("保存数据5秒测试:")
-                        .append(check("测试数据1", CacheUtil.get("key13")))
+                        .append(check("测试数据1", key13Value))
                         .append("\n")
                         .append("保存对象数据5秒测试:")
-                        .append(check(new Test(1, "2").toString(), strKey14))
+                        .append(check(new Test(1, "2").toString(), key14Value))
                         .append("\n")
                         .append("加密保存数据5秒测试:")
-                        .append(check("测试数据1", CacheUtil.get("key15", true)))
+                        .append(check("测试数据1", key15Value))
                         .append("\n")
                         .append("加密保存对象数据5秒测试:")
-                        .append(check(new Test(1, "2").toString(), strKey16))
+                        .append(check(new Test(1, "2").toString(), key16Value))
                         .append("\n")
                         .append("key加密保存数据测试:")
-                        .append(check("123456",
-                                CacheUtil.get(CacheUtil.translateKey("key17"), true)))
+                        .append(check("123456", key17Value))
                         .append("\n")
                         .append("未保存数据测试(Boolean类型):")
-                        .append(CacheUtil.get(CacheUtil.translateKey("null"), Boolean.class, false,
-                                true))
+                        .append(key19Value)
                         .append("\n")
                         .append("未保存数据测试(Test类):")
-                        .append(CacheUtil.get(CacheUtil.translateKey("null1"), Test.class,
-                                new Test(1, "默认"), true).toString())
+                        .append(key20Value.toString())
                         .append("\n")
                         .toString();
                 mTextView.setText(value);
